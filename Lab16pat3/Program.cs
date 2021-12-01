4f764ba078121810433a9ec96f3cd1d210cee215
Татьяна Анисимова <kulmina@mail.ru>
2021-12-01 13:08:38 +0300

    456

--- Lab16pat3/Program.cs
+++ Lab16pat3/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Lab16pat3
 {
@@ -15,11 +16,22 @@ namespace Lab16pat3
             string jsonString;
             jsonString = File.ReadAllText("person1.json");
             Console.WriteLine(jsonString);
-           // string jsonString1 = "{\"Код товара\": 1, \"Вид\": \"Цветы\",\"Цена\": 500,\"Наименование\": \"Фиалка\"},{\"Код товара\": 2,\"Вид\": \"Цветы\",\"Цена\": 600,\"Наименование\": \"Сирень\"},{\"Код товара\": 3,\"Вид\":\"Цветы\",\"Цена\": 700,\"Наименование\": \"Ромашка\"},{\"Код товара\": 4,\"Вид\": \"Цветы\",\"Цена\": 800,\"Наименование\": \"Роза\"},{\"Код товара\": 5,\"Вид\": \"Цветы\",\"Цена\": 900,\"Наименование\": \"Орхидея\"}";
-            char[] products = jsonString.ToArray();
-
+            string jsonString1 = "[{\"Код товара\": 1, \"Вид\": \"Цветы\",\"Цена\": 500,\"Наименование\": \"Фиалка\"},{\"Код товара\": 2,\"Вид\": \"Цветы\",\"Цена\": 600,\"Наименование\": \"Сирень\"},{\"Код товара\": 3,\"Вид\":\"Цветы\",\"Цена\": 700,\"Наименование\": \"Ромашка\"},{\"Код товара\": 4,\"Вид\": \"Цветы\",\"Цена\": 800,\"Наименование\": \"Роза\"},{\"Код товара\": 5,\"Вид\": \"Цветы\",\"Цена\": 900,\"Наименование\": \"Орхидея\"}]";
+            
+            Product product = JsonSerializer.Deserialize<Product>(jsonString1);
             Console.ReadKey();
         }
+    }
+    class Product
+    {
+        [JsonPropertyName("Код товара")]
+        public int Code { get; set; }
+        [JsonPropertyName("Вид")]
+        public string TipeProduct { get; set; }
+        [JsonPropertyName("Цена")]
+        public decimal Price { get; set; }
+        [JsonPropertyName("Наименование")]
+        public string Name { get; set; }
     }
 }
 
@@ -76,5 +88,5 @@ namespace Lab16pat3
 
 //    Sum += newarrey[j];
 //}
-
+// char[] products = jsonString.ToArray();
 
