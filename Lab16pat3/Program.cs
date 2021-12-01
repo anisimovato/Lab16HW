221558a34febc67c7b8a90064b3289e98f388c5a
Татьяна Анисимова <kulmina@mail.ru>
2021-12-01 15:08:35 +0300

    на проверку

--- Lab16pat3/Program.cs
+++ Lab16pat3/Program.cs
@@ -29,7 +29,7 @@ namespace Lab16pat3
                     name = prod.Name;
                 }
             }
-            Console.WriteLine($"Самый дорогой продукт", (name)); 
+            Console.WriteLine("Самый дорогой продукт,{0}",name); 
             Console.ReadKey();
         }
     }
@@ -48,58 +48,4 @@ namespace Lab16pat3
     }
 }
 
-
-//string jsonString = "{\"firstName\":\"Tom\",\"lastName\": \"Jekson\", \"gender\" : \"male\",\"age\":29,\"onlain\":true,\"hobby\":[ \"footbol\",\"reading\",\"swimming\"]}";
-
-//[
-//  {
-//    "Код товара": 1,
-//    "Вид": "Цветы",
-//    "Цена": 500,
-//    "Наименование": "Фиалка"
-//  },
-//  {
-//    "Код товара": 2,
-//    "Вид": "Цветы",
-//    "Цена": 600,
-//    "Наименование": "Сирень"
-//  },
-//  {
-//    "Код товара": 3,
-//    "Вид": "Цветы",
-//    "Цена": 700,
-//    "Наименование": "Ромашка"
-//  },
-//  {
-//    "Код товара": 4,
-//    "Вид": "Цветы",
-//    "Цена": 800,
-//    "Наименование": "Роза"
-//  },
-//  {
-//    "Код товара": 5,
-//    "Вид": "Цветы",
-//    "Цена": 900,
-//    "Наименование": "Орхидея"
-//  }
-//]
-
-
-// products = JsonSerializer.Deserialize<Product>(jsonString);
-//StreamReader sr = new StreamReader("person1.json");
-//string products = sr.ReadLine();
-//foreach (var number in products.Split())
-//{
-//    Console.WriteLine(number);
-//}
-//char[] newarrey = products.ToArray();
-
-//Person person = JsonSerializer.Deserialize<Person>(jsonString)
-//int Sum = 0;
-////for (int j = 0; j < newarrey.Length; j++)
-//{
-
-//    Sum += newarrey[j];
-//}
-// char[] products = jsonString.ToArray();
 
